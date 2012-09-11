ec0995558ae8ff775d25595c8aced7d6883d0c28
Joubin Houshyar <alphazero@sensesay.net>
2012-09-11 09:49:15 -0400

    ADD - go doc header

--- gestalt.go
+++ gestalt.go
@@ -2,6 +2,101 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Gestalt defines a basic configuration utility object supporting
+// string, []string, and map[string]string configuration keys. Instances are typically
+// created by the user provided configuration specs
+// (or files), but it is also possible to directly add entries view the
+// via underlying map[string]interface{}.
+//
+// Both keys and values may have embedded whitespace (spaces or tabs) and
+// quoted values support leading and/or trailing whitespace in values.
+// Beyond that, mainly note that the `=` character is reserved and can not be
+// used in keys.
+//
+// Single line and trailing comments are supported.
+//
+// Gestalt provides a semantic interface for access to configuration elements,
+// but can be access in a raw format via its underlying map[string]interface{}.
+// It follows that you can also add entries to a gestalt instance via the map
+// semantics.  In that case, no type enforcement is provided, e.g. you can add
+// a []string bound to a key not ending in "[]", but you will not be able to
+// retrieve via GetArray as the semantic methods do check against key.  It is
+// possible to reflectively address this, but the added complexity for a corner
+// use-case, and the reflection overhead for general usecase argues against doing so.
+//
+// Example:
+//
+//  # A canonical Gestalt spec:
+//
+//  # -------------------------------------------
+//  # demonstrate string type properties
+//  # -------------------------------------------
+//
+//  # basic - note leading and trailing whitespace (including tabs)
+//  # are removed from both keys and values.
+//  # also note the trailing comment form.
+//  prop one=prop one value                 # "prop one" => "prop one value"
+//  another property   =  value             # "another property" => "value"
+//
+//  # use quotes for leading or trailing spaces/tabs in values
+//  log.info.level.id = "INFO "             # "log.info.level.id" => "INFO "
+//  leading.whitespace = " test"            # "leading.whitespace" => " test"
+//
+//  # multiline values use the '\' line continuation char.
+//  # note that ALL leading characters on a continued line are appended to value.
+//  # and any trailing white space chars immediately before `\` are also appended.
+//  long one = This sentence ends \
+//  in 4 spaces\
+//      .                                   # "long one" => "This sentence ends in 4 spaces    ."
+//
+//  zerovalue =                             # "zerovalue" => ""
+//
+//  # -------------------------------------------
+//  # demonstrate []string type properties
+//  # any key ending in "[]" (and not "map[]")
+//  # is treated as a []string type
+//  # -------------------------------------------
+//
+//  # note that whitespace between `,` is removed.
+//  an array [] = 1 , 2 , 3                 # "an array []" => ["1" "2" "3"]
+//  another.array[] = "  1" , " 20", 300    # "an array []" => ["  1" " 20" "300"]
+//
+//  # note that for arrays (and maps) the leading and trailing
+//  # white space for individual values on continued lines
+//  # are trimmed (that is if a `,' precedes the `\`.
+//  # Also note tha
+//  multi-line[] = a, b, c, \
+//                 12\
+//   4567  ,\
+//                 d, e         # "multi-line[]" => ["a" "b" "c" "12 4567" "d" "e"]
+//
+//  another.one[] = \
+//      a, \
+//      b, \
+//      c                       # "another.one[]" => ["a" "b" "c"]
+//
+//  # you can define empty []string properties
+//  empty[] =                   # "empty[]" => []
+//
+//
+//  # -------------------------------------------
+//  # demonstrate map[string]string type properties
+//  # e.g. any key ending in "map[]"
+//  # -------------------------------------------
+//  a map[] = a:1 , b:2, c : 3 , d:4         # "a map[]" => map[a:1 c:3 b:2 d:4]
+//
+//  # maps can be defined in multi-lines per usual patterns and WS considerations
+//  multline.map[] = \
+//    a:1 , \
+//    b:2, c:3, \
+//    d:4                                    # "multline.map[]" => map[a:1 c:3 b:2 d:4]
+//
+//  # maps can have zerovalue entries
+//  zv.entry.map[] =  foo:bar, zerovalue:    # "zv.entry.map[]" => map[foo:bar zerovalue:]
+//
+//  # and finally, you can define empty map[string]string, as well.
+//  empty.map[] =                            # "empty.map[]" => map[]
+//
 package gestalt
 
 import (
@@ -20,21 +115,17 @@ type Gestalt map[string]interface{}
 // Create a new Gestalt object from spec.
 // If spec is zero value string, it simply returns an empty intances.
 func New(spec string) (Gestalt, error) {
-	//	if spec == "" {
-	//		return nil, fmt.Errorf("spec is zerovalue")
-	//	}
 
 	gestalt, e := parse(spec)
 	if e != nil {
 		return nil, e
 	}
 
-	for k, v := range gestalt {
-		fmt.Printf("DEBUG <%s> => <%s>\n", k, v)
-	}
 	return gestalt, nil
 }
 
+// Creates a new Gestalt instance from the specified file.
+// Returns error if fname is zerovalue or on io errors.
 func ReadFile(fname string) (g Gestalt, e error) {
 	if fname == "" {
 		e = fmt.Errorf("fname is zero-value")
@@ -47,6 +138,11 @@ func ReadFile(fname string) (g Gestalt, e error) {
 	}
 
 	return New(string(b))
+}
+
+// Just for completeness sake -- you can always use gestalk[key].
+func (g Gestalt) Get(key string) interface{} {
+	return g[key]
 }
 
 func (g Gestalt) GetOrDefault(key string, defvalue interface{}) (value interface{}) {
